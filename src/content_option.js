4ea14d89e547c105b7fda8a941b9ca59fe278a27
Shan-3021 <106008753+Shan-3021@users.noreply.github.com>
2022-05-24 21:29:58 +0530

    Update content_option.js

--- src/content_option.js
+++ src/content_option.js
@@ -1,6 +1,6 @@
-const logotext = "JOHN";
+const logotext = "Sanmathi";
 const meta = {
-    title: "John Doe",
+    title: "Sanmathi",
     description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
 };
 
@@ -11,7 +11,7 @@ const introdata = {
         second: "I code cool websites",
         third: "I develop mobile apps",
     },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    description: "Lorem ipsum dolor sit amett, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
     your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
 };
 
